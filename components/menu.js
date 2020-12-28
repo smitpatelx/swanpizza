4d2ee2a5559d1b9241d0c61d26b04053998aebcd
Smit Patel <smitbpatel1999@gmail.com>
2020-12-27 23:49:49 -0600

    Social media links added

--- components/menu.js
+++ components/menu.js
@@ -6,11 +6,11 @@ export default function Menu(props) {
     }
     return (
         <div>
-            <div className="absolute top-10 z-20 left-1/2 px-2 pb-2 transform -translate-x-1/2 w-5/6 bg-white rounded-xl">
+            <div className="absolute top-4 z-20 left-1/2 px-2 pb-2 transform -translate-x-1/2 w-5/6 bg-white rounded-xl">
                 <div className="w-full flex flex-wrap justify-center items-center py-6">
                     <a className="py-3 px-6 bg-green-swan hover:bg-opacity-75 text-white text-lg font-semibold shadow-lg rounded-lg focus:outline-none focus:ring focus:ring-green-200 transition-all duration-300" href="/menu.pdf">Download Menu</a>
                 </div>
-                <iframe src="/combinepdf-min.pdf" width="100%" height="800px" style={{borderRadius:"15px"}}/>
+                <iframe src="/combinepdf-min.pdf" width="100%" height="600px" style={{borderRadius:"15px"}}/>
             </div>
             <div onClick={exitPopup} className="w-full z-0 h-full min-h-screen overlay-app absolute top-0 left-0"></div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -26,15 +26,15 @@ export default function Home() {
           Red Swan <span className="text-green-swan">Pizza</span>
         </h1>
         <h2 className="text-xl font-medium text-gray-700 w-full mt-2 flex flex-wrap items-center justify-start">
-          <svg className="w-8 h-8 inline-block mr-2" fill="currentColor" viewBox="0 0 50 50"><defs/><path fill-rule="evenodd" d="M39.583 12.5H10.417a2.09 2.09 0 01-2.084-2.083 2.09 2.09 0 012.084-2.083h29.166a2.09 2.09 0 012.084 2.083 2.09 2.09 0 01-2.084 2.083zM42 16.25a2.079 2.079 0 00-2.042-1.666H10.042c-1 0-1.854.708-2.042 1.666L6.25 25v2.084a2.09 2.09 0 002.083 2.083v10.416a2.09 2.09 0 002.084 2.084h16.666a2.09 2.09 0 002.084-2.084V29.168H37.5v10.416a2.09 2.09 0 002.083 2.084 2.09 2.09 0 002.084-2.084V29.168a2.09 2.09 0 002.083-2.084V25L42 16.25zM25 37.5H12.5v-8.333H25V37.5z" clip-rule="evenodd"/></svg>
+          <svg className="w-8 h-8 inline-block mr-2" fill="currentColor" viewBox="0 0 50 50"><defs/><path fillRule="evenodd" d="M39.583 12.5H10.417a2.09 2.09 0 01-2.084-2.083 2.09 2.09 0 012.084-2.083h29.166a2.09 2.09 0 012.084 2.083 2.09 2.09 0 01-2.084 2.083zM42 16.25a2.079 2.079 0 00-2.042-1.666H10.042c-1 0-1.854.708-2.042 1.666L6.25 25v2.084a2.09 2.09 0 002.083 2.083v10.416a2.09 2.09 0 002.084 2.084h16.666a2.09 2.09 0 002.084-2.084V29.168H37.5v10.416a2.09 2.09 0 002.083 2.084 2.09 2.09 0 002.084-2.084V29.168a2.09 2.09 0 002.083-2.084V25L42 16.25zM25 37.5H12.5v-8.333H25V37.5z" clipRule="evenodd"/></svg>
           2-2401 McPhillips Street - Winnipeg, R2V3M7
         </h2>
         <div className="w-full flex flex-wrap items-center justify-start mt-5 space-x-6">
-          <a href="tel:+12043066666" className="text-white bg-green-swan hover:bg- rounded-full py-3 pl-6 pr-8 text-xl font-semibold focus:outline-none focus:ring focus:ring-green-400 transition-all duration-300">
-            <svg className="w-6 h-6 inline-block mr-1" fill="currentColor" viewBox="0 0 30 30"><defs/><path d="M24.027 19.085l-3.175-.363a2.488 2.488 0 00-2.05.713l-2.3 2.3a18.807 18.807 0 01-8.237-8.237l2.312-2.313c.538-.537.8-1.288.713-2.05l-.363-3.15A2.502 2.502 0 008.44 3.772H6.277c-1.412 0-2.587 1.175-2.5 2.588.663 10.675 9.2 19.2 19.863 19.862 1.412.088 2.587-1.087 2.587-2.5V21.56a2.477 2.477 0 00-2.2-2.475z"/></svg>
+          <a href="tel:+12043066666" className="text-white bg-green-swan hover:bg- rounded-full py-3 pl-6 pr-8 text-lg font-semibold focus:outline-none focus:ring focus:ring-green-400 transition-all duration-300">
+            <svg className="w-5 h-5 inline-block mr-1" fill="currentColor" viewBox="0 0 30 30"><defs/><path d="M24.027 19.085l-3.175-.363a2.488 2.488 0 00-2.05.713l-2.3 2.3a18.807 18.807 0 01-8.237-8.237l2.312-2.313c.538-.537.8-1.288.713-2.05l-.363-3.15A2.502 2.502 0 008.44 3.772H6.277c-1.412 0-2.587 1.175-2.5 2.588.663 10.675 9.2 19.2 19.863 19.862 1.412.088 2.587-1.087 2.587-2.5V21.56a2.477 2.477 0 00-2.2-2.475z"/></svg>
             Call Us
           </a>
-          <button onClick={menuToggle} type="button" className="text-green-swan bg-transparent rounded-full py-3 px-6 text-xl font-semibold focus:outline-none focus:ring focus:ring-green-400 transition-all duration-300 border-2 border-green-swan">
+          <button onClick={menuToggle} type="button" className="text-green-swan bg-transparent rounded-full py-3 px-6 text-lg font-semibold focus:outline-none focus:ring focus:ring-green-400 transition-all duration-300 border-2 border-green-swan">
             Our Menu
           </button>
         </div>
@@ -44,19 +44,19 @@ export default function Home() {
       </div>
       <div className="w-full flex flex-wrap justify-start items-stretch space-x-24 pt-4 pb-12 pl-16 mt-2">
         <div className={styles.dealCards}>
-          <h4>Deal #1</h4>
+          <h4>Deal <b>1</b></h4>
           <h1>1 Personal 8"</h1>
           <h3>1 Can of POP</h3>
           <h2>$ 5.99</h2>
         </div>
         <div className={styles.dealCards}>
-          <h4>Deal #2</h4>
+          <h4>Deal <b>2</b></h4>
           <h1>1 Small 10"</h1>
           <h3>Extra $ for Keto, Cauliflower<br/>and Gluten free crust</h3>
           <h2>$ 8.45</h2>
         </div>
         <div className={styles.dealCards}>
-          <h4>Deal #3</h4>
+          <h4>Deal <b>3</b></h4>
           <h1>1 Large 14"</h1>
           <h3>2 Toppings</h3>
           <h2>$ 10.95</h2>
